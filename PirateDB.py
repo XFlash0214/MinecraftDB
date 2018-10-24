ec913a493ea48ff9c1b68f38842ab0f8365c2e20
Sampson Neyman <43590651+XFlash0214@users.noreply.github.com>
2018-10-23 19:22:07 -0500

    Add files via upload

--- PirateDB.py
+++ PirateDB.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import json
 class Pirate:
     name=""
     ship=""
@@ -13,3 +15,50 @@ class Pirate:
         self.name=d["name"]
         self.ship=d["ship"]
         self.real=d["real"]
+class FileManger:
+    path="PirateDB.json"
+    def wtf(self, idnum, obj):
+        try:
+            f=open(self.path,"r")
+            d=json.load(f)
+            f.close()
+        except:
+            d={}
+        d[idnum]=obj
+        f=open(self.path,"w")
+        json.dump(d,f)
+        f.close()
+root=Tk()
+root.title("Pirate Database")
+root.config(bg="pink")
+
+title=Label(root,text="Pirate Database", font="Arial 30 bold",bg="pink")
+title.grid(row=0,column=0,columnspan=3)
+
+nametk=Label(root,text="Name:", font="Arial 20",bg="pink")
+nametk.grid(row=1,column=0,columnspan=1)
+
+namebox=Entry(root, font="Arial 20")
+namebox.grid(row=1,column=2)
+
+shiptk=Label(root,text="Ship:", font="Arial 20",bg="pink")
+shiptk.grid(row=2,column=0,columnspan=1)
+
+shipbox=Entry(root, font="Arial 20")
+shipbox.grid(row=2,column=2)
+
+realtk=Label(root,text="Real:", font="Arial 20",bg="pink")
+realtk.grid(row=3,column=0,columnspan=1)
+
+optionString=StringVar(root)
+optionString.set("True")
+dropdown=OptionMenu(root,optionString,"True","False")
+dropdown.config(font="Arial 20",width="10")
+dropdown.nametowidget(dropdown.menuname).config(font="Arial 20")
+dropdown.grid(row=3,column=2)
+
+
+save=Button(root,text="SAVE",font="Arial 20 bold",bg="red")
+save.grid(row=4,column=1,columnspan=2)
+
+root.mainloop()
